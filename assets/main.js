479807169267e3e0d80a0c90eaf1506b184b8f23
Jay Bienvenu <github@bienv.com>
2015-06-04 21:54:36 -0500

    Add content to the page.

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+﻿requirejs.config({
+    paths: {
+        'text': '../lib/require/text',
+        'bootstrap': '../lib/bootstrap/js/bootstrap',
+        'jquery': '../lib/jquery/jquery-1.9.1'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'jQuery'
+       }
+    }
+});
